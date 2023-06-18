6094778ef8195552aefc938a02b2279fa16fcbda
Vivian Yam <lovecheetahs101@gmail.com>
2023-06-17 23:52:54 -0400

    created the type-writer effect and changed font for project

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ const handleScroll = () => {
 
 window.addEventListener('scroll', handleScroll);
 
+
+//typewriter effect
+
+var messageArray = ["Hey, welcome to my website! My name is Vivian Yam!"];
+var textPosition = 0;
+var speed = 100;
+
+typewriter = () => {
+  document.querySelector("#message").
+  innerHTML = messageArray[0].substring(0,textPosition) + "<span>\u25ae</span>";
+
+  if (textPosition++ != messageArray[0].length)
+    setTimeout(typewriter, speed);
+}
+window.addEventListener("load", typewriter);
 // fade in animation on the about me section
 
 
